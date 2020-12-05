bf5b779ae48cb11b07cd8fb63d111ee9ecadb5e2
Koyashiro Kohaku <develop@koyashiro.net>
2020-12-05 16:50:34 +0900

    feat: add `exercise-errors.go`

---
+++ methods/exercise-errors/exercise-errors.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type ErrNegativeSqrt float64
+
+func (e ErrNegativeSqrt) Error() string {
+	return fmt.Sprintf("cannot Sqrt negative number: %g", float64(e))
+}
+
+func Sqrt(x float64) (float64, error) {
+	if x < 0 {
+		return 0, ErrNegativeSqrt(x)
+	}
+	z := 1.
+	diff := 0.
+	for i := 0; i < 10; i++ {
+		diff = (z*z - x) / (2 * z)
+		z -= diff
+
+		if math.Abs(diff) < 1e-10 {
+			break
+		}
+	}
+	return z, nil
+}
+
+func main() {
+	fmt.Println(Sqrt(2))
+	fmt.Println(Sqrt(-2))
+}
